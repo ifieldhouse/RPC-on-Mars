11d8cd41be9bb1bf35e67ba653e189952acf4e6e
ifieldhouse <ifieldhouse@uc.cl>
2019-10-16 16:24:40 -0300

    Reduce test interface

--- test.py
+++ test.py
@@ -1,57 +1,20 @@
-from rpc import RPC
+#!/usr/bin/env python3
+
+import rpc
 
 
-rpc = RPC()
+lib = rpc.RPC()
 username = None
 
-while True:
+prompt = f'¿Qué deseas hacer, {username}?' if username is not None else '¿Qué deseas hacer?'
 
-    prompt = f'¿Qué deseas hacer, {username}?' if username is not None else '¿Qué deseas hacer?'
+print(prompt)
+print(lib)
+option = input('Escriba su opción: ')
 
-    option = input(f"""
-{prompt}
-(1) Ver mensajes en 'General'
-(2) Enviar mensaje a 'General'
-(3) Definir su nombre
-(4) Ver tus mensajes
-(5) Ver usuarios disponibles
-(6) Enviar mensaje a usuario
-(7) Salir
-Escriba su opción: """)
+if option == '1':
+    ans = lib.add(2, b=3)
+    print(ans)
 
-    if option == '1':
-        logs = rpc.get_logs()
-        for log in logs:
-            print(log)
-
-    elif option == '2':
-        msg = input('Escribe el mensaje a general: ')
-        rpc.write_to_log(msg)
-
-    elif option == '3':
-        msg = input('Escriba su nombre: ')
-        username = msg
-        user = username
-        rpc.set_name(msg)
-
-    elif option == '4':
-        if username == None:
-            print('Usted es anónimo; no tiene mensajes.')
-        else:
-            logs = rpc.get_my_logs(username)
-            for log in logs:
-                print(logs)
-
-    elif option == '5':
-        users = rpc.get_users()
-        for user in users:
-            print(user)
-
-    elif option == '6':
-        target = input('Nombre del destinatario: ')
-        msg = input('Mensaje a enviar: ')
-        rpc.write_to_client_log(target, msg)
-
-    elif option == '7':
-        print('Saliendo del programa...')
-        break
+elif option == '0':
+    print('Saliendo del programa...')
